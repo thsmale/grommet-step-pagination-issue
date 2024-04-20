3b8b7f2d3fcc290dca6f2367e34c8a08d8f36a89
thsmale <tsmale@gmail.com>
2024-04-19 17:14:58 -0700

    update with minimum replicable example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import {
   Box,
 	Grommet,
+	Pagination,
+	Select,
   Text,
 } from "grommet";
 
 const App = () => {
-
+	const [limit, setLimit] = useState(10);
   return (
     <Grommet>
-			<Box>
-				<Text>Hello world</Text>
+			<Box gap='medium'>
+				<Text>Use the Select component to adjust the step used in the Pagination component. However, it fails to update the pagination endIndex.</Text>
+				<ul>
+					<li>10 should be 1 2 3 4 5 ... 100</li>
+					<li>25 should be 1 2 3 4 5 ... 40</li>
+					<li>50 should be 1 2 3 4 5 ... 20</li>
+					<li>100 should be 1 2 3 4 5 ... 10</li>
+				</ul>
+				<Select
+					options={[10, 25, 50, 100]}
+					value={limit}
+					onChange={({ option }) => setLimit(option)}
+				/>
+				<Pagination
+					numberItems={1000}
+					step={limit}
+				/>
 			</Box>
     </Grommet>
   );
